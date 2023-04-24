677809e71546e9a39176d2116ff4870fc24dd6dd
Tyler Eisen <jteisen90@gmail.com>
2023-04-24 16:19:29 +0700

    Added kennel.sql and adjusted the get all and get single requests

--- views/employee_requests.py
+++ views/employee_requests.py
@@ -1,3 +1,7 @@
+import json
+import sqlite3
+from models import Employee
+
 EMPLOYEES = [
     {
         "id": 1,
@@ -12,17 +16,57 @@ EMPLOYEES = [
 ]
 
 def get_all_employees():
-  return EMPLOYEES
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.address
+        FROM employee a
+        """)
+
+        
+        employees = []
+
+        
+        dataset = db_cursor.fetchall()
+        for row in dataset:
+
+        
+            employee = Employee(row['id'], row['name'], row['address'])
+
+            employees.append(employee.__dict__) 
+
+    return employees
 
 
 def get_single_employee(id):
-    requested_employee = None
-  
-    for employee in EMPLOYEES:
-     if employee["id"] == id:
-      requested_employee = employee
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
 
-    return requested_employee
+        
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.name,
+            a.address
+        FROM employee a
+        WHERE a.id = ?
+        """, ( id, ))
+
+        
+        data = db_cursor.fetchone()
+
+        
+        employee = Employee(data['id'], data['name'], data['address'],
+                            )
+
+        return employee.__dict__
   
 def delete_employee(id):
     employee_index = -1
